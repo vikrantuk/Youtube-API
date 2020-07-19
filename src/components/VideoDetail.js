8a6a9324a8948b092220a02673471cac14989890
vikrant karawande <viku1996@gmail.com>
2020-07-19 14:04:20 +0530

    Fixed warnings

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,7 +7,7 @@ const VideoDetail = ({video}) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe src={`https://www.youtube.com/embed/${video.id.videoId}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe title="video player" src={`https://www.youtube.com/embed/${video.id.videoId}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
